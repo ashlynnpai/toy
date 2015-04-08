15b5a73f6c5b9b85960ea0143cd2a52f98641058
ashlynnpai <ashlynnpai@gmail.com>
2015-04-08 06:08:54 +0000

    layout and facebook js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,14 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
-  helper_method :current_user
+  def require_user
+    redirect_to login_path unless current_user
+   end
+  
+  def logged_in?
+    !!current_user
+  end  
+  
+  helper_method :current_user, :logged_in?, :require_use
 end
+
