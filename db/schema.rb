31187609faa636eb66d16b442014503b2b0784ea
ashlynnpai <ashlynnpai@gmail.com>
2015-04-13 15:24:34 +0000

    user table and sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325164258) do
+ActiveRecord::Schema.define(version: 20150413151229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150325164258) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "available",                            default: true
+  end
+
+  create_table "users", force: true do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
