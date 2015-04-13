4ee7466d14f7dd011415cb1cab242ff45e7c69f3
ashlynnpai <ashlynnpai@gmail.com>
2015-04-13 16:00:45 +0000

    facebook policy, oauth user

---
+++ app/models/o_auth_user.rb
@@ -0,0 +1,19 @@
+class OAuthUser
+
+  attr_reader :provider, :user
+
+  def initialize creds, user = nil
+    @auth         = creds
+    @user         = user
+    @provider     = @auth.provider
+    @policy       = "#{@provider}_policy".classify.constantize.new(@auth)
+  end
+
+  def login_or_create
+    logged_in? ? create_new_account : (login || create_new_account)
+  end
+
+  def logged_in?
+    @user.present?
+  end
+end
